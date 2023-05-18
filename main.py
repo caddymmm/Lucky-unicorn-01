29ff7e5cdc4ef1a31a3fe15273905926835b4463
caddymmm <132413731+caddymmm@users.noreply.github.com>
2023-05-18 13:34:09 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+#users for a | er
+get_number = int(input ("Choose a number? "))
+# Multiply the number by 5
+times_five = get_number * 5
+# output the result
+answer = ("{} times 5 equal to {}".format(get_number, times_five))
+print(answer)
